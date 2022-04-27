d203a4e183062d84fa83efca05c23e25b18c0d4b
faboroa <faboroa@coventry.ac.uk>
2022-04-27 14:40:56 +0000

    Set response headers to be the first on app.use in middleware

--- api/middleware.js
+++ api/middleware.js
@@ -171,7 +171,7 @@ async function setHeaders(context, next) {
 	context.response.headers.set('charset', 'utf-8')
 	context.response.headers.set('Access-Control-Allow-Origin', '*')
 	context.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-	context.response.headers.set('Access-Control-Allow-Headers', '*')
+	context.response.headers.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 	context.response.headers.set('Access-Control-Allow-Credentials', true)
 	await next()
 }
@@ -184,8 +184,8 @@ async function defaultResponse(context) {
 	context.response.body = data
 }
 
-app.use(errorHandler)
 app.use(setHeaders)
+app.use(errorHandler)
 app.use(staticFiles)
 app.use(checkContentType)
 app.use(authHeaderPresent)
